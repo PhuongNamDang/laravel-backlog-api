1d6fce2e879d3577a96ef0ae535992b887be0d89
CBA Nick <nick@cybridge.jp>
2021-03-16 18:11:03 +0700

    fix autoload

--- config/config.php
+++ config/config.php
@@ -6,11 +6,4 @@ return [
     'secret' => env('BACKLOG_SECRET', ''),
 
     'domain' => env('BACKLOG_DOMAIN', ''),
-
-    'providers' => [
-        Itigoppo\BacklogApi\Providers\LaravelServiceProvider::class,
-    ],
-    'aliases' => [
-        'BacklogApi' => Itigoppo\BacklogApi\Facades\Backlog::class,
-    ]
 ];
